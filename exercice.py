121e1b42d56a293bfec72c1d225236cb192635b1
carl salem <71038407+Flextapes@users.noreply.github.com>
2020-10-07 12:47:12 -0700

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import math
 # TODO: Importez vos modules ici
-
-
+def ellipse(mass_volum, a = 8, b = 9, c = 22):
+    volume = 4/3 * (math.pi) * a * b * c
+    mass = volume/mass_volum
+    
+    return volume, mass
+    
 
 # TODO: Définissez vos fonction ici
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    volume, mass = ellipse(a = 10, b = 22, c = 33)
+    print (volume, mass)
 
-    pass
